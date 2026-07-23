d5a65dd8557b8d6a00335c4124488a94c799c896
Lipa <286813590+xak-koye@users.noreply.github.com>
2026-07-23 22:23:57 +0300

    Offline

--- app/src/main/java/com/bizane/app/BizaneApp.kt
+++ app/src/main/java/com/bizane/app/BizaneApp.kt
@@ -2,12 +2,16 @@ package com.bizane.app
 
 import android.app.Application
 import com.bizane.app.data.FoodStorage
+import com.bizane.app.data.LocalTrashStorage
 import com.bizane.app.data.Prefs
+import com.bizane.app.data.Storage
 
 class BizaneApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Prefs.init(this)
+        Storage.init(this)
         FoodStorage.load()
+        LocalTrashStorage.load()
     }
 }
